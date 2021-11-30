5a228948eb0d7cf6fe203f18d1043bcc1ab76d95
Рубинова Полина <54933428+PolinaRubinova@users.noreply.github.com>
2021-11-30 22:19:37 +0300

    Add files via upload

---
+++ lab5/back.sh
@@ -0,0 +1,5 @@
+echo "hello"
+read $b
+echo "wussup"
+echo "im very tired of it"
+sleep 7
---
+++ lab5/fore.sh
@@ -0,0 +1,5 @@
+echo "whats your name?"
+read a
+echo "hi, $a! glad to see you"
+sleep 5
+echo "goodbye, friend!"
